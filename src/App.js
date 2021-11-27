58f75e8a5fb9bdfa6257af4171ebafd52e02935a
Akshay Bhopani <akshaybhopani@gmail.com>
2021-11-27 15:34:21 +0530

    Uploading Files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+//Importing Components 
+
+import Navbar from './Components/Navbar'
+import Main from './Components/Main'
+import Footer from './Components/Footer'
+
+function App() {
+  return (
+   <>
+   <Navbar/>
+   <Main/>
+   <Footer/>
+   </>
+  );
+}
+
+export default App;
